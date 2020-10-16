8659d6d5e2487282ae716d2ad212e358dad100e9
Louis <lcastric@edu.uwaterloo.ca>
2020-10-16 18:05:12 -0400

    Debugging

--- main.py
+++ main.py
@@ -71,11 +71,11 @@ for i in range(0, 35):
     print(list(data["test"].keys())[i])
     print(list(data["test"].values())[i])
     inp_text = list(data["test"].keys())[i]
-    print(tokenizer.encode('<blank>', add_prefix_space=False))
-    print(tokenizer.encode(list(data["test"].keys())[i]))
-    sys.exit()
+    #print(tokenizer.encode('<blank>', add_prefix_space=False))
+    #print(tokenizer.encode(list(data["test"].keys())[i]))
+    #sys.exit()
     inputs = tokenizer([inp_text], max_length=1024, return_tensors='pt').to('cuda')
     outputs = model.generate(inputs['input_ids'], num_beams=20, max_length=35,\
-        early_stopping=True, repetition_penalty=2.0, bad_words_ids=[tokenizer.encode("<blank>", add_prefix_space=False)],\
+        early_stopping=True, repetition_penalty=2.0, bad_words_ids=[[50278]],\
         no_repeat_ngram_size=4)
     print('Generated: {}'.format(tokenizer.decode(outputs[0], skip_special_tokens=True)))
