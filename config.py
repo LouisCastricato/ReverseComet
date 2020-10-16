350afa341c8b8fe5a9fa4efe5d39c4cac70e8740
Louis <lcastric@edu.uwaterloo.ca>
2020-10-16 14:58:35 -0400

    fix

--- config.py
+++ config.py
@@ -33,73 +33,32 @@ class Seq2SeqTrainingArguments(TrainingArguments):
         predict_with_generate (:obj:`bool`, `optional`, defaults to :obj:`False`):
             Whether to use generate to calculate generative metrics (ROUGE, BLEU).
     """
-
-    label_smoothing = field(
-        default=0.0, metadata={"help": "The label smoothing epsilon to apply (if not zero)."}
-    )
-    sortish_sampler = field(default=False, metadata={"help": "Whether to SortishSamler or not."})
-    predict_with_generate = field(
-        default=False, metadata={"help": "Whether to use generate to calculate generative metrics (ROUGE, BLEU)."}
-    )
-    adafactor = field(default=False, metadata={"help": "whether to use adafactor"})
-    encoder_layerdrop = field(
-        default=None, metadata={"help": "Encoder layer dropout probability. Goes into model.config."}
-    )
-    decoder_layerdrop = field(
-        default=None, metadata={"help": "Decoder layer dropout probability. Goes into model.config."}
-    )
-    dropout = field(default=None, metadata={"help": "Dropout probability. Goes into model.config."})
-    attention_dropout = field(
-        default=None, metadata={"help": "Attention dropout probability. Goes into model.config."}
-    )
-    lr_scheduler = field(
-        default="linear"
-    )
+    def __init__(self):
+        self.label_smoothing = 0.0 
+        self.sortish_sampler = False 
+        self.predict_with_generate = False
+        self.adafactor = False
+        self.encoder_layerdrop = None
+        self.decoder_layerdrop = None
+        self.dropout = None
+        self.attention_dropout = None
+        self.lr_scheduler = "linear"
 
 @dataclass
 class DataTrainingArguments:
     """
     Arguments pertaining to what data we are going to input our model for training and eval.
     """
-
-    data_dir = field(
-        metadata={"help": "The input data dir. Should contain the .tsv files (or other data files) for the task."}
-    )
-    task = field(
-        default="summarization",
-        metadata={"help": "Task name, summarization (or summarization_{dataset} for pegasus) or translation"},
-    )
-    max_source_length = field(
-        default=1024,
-        metadata={
-            "help": "The maximum total input sequence length after tokenization. Sequences longer "
-            "than this will be truncated, sequences shorter will be padded."
-        },
-    )
-    max_target_length  = field(
-        default=128,
-        metadata={
-            "help": "The maximum total sequence length for target text after tokenization. Sequences longer "
-            "than this will be truncated, sequences shorter will be padded."
-        },
-    )
-    val_max_target_length  = field(
-        default=142,
-        metadata={
-            "help": "The maximum total sequence length for validation target text after tokenization. Sequences longer "
-            "than this will be truncated, sequences shorter will be padded."
-        },
-    )
-    test_max_target_length = field(
-        default=142,
-        metadata={
-            "help": "The maximum total sequence length for test target text after tokenization. Sequences longer "
-            "than this will be truncated, sequences shorter will be padded."
-        },
-    )
-    n_train  = field(default=-1, metadata={"help": "# training examples. -1 means use all."})
-    n_val  = field(default=-1, metadata={"help": "# validation examples. -1 means use all."})
-    n_test  = field(default=-1, metadata={"help": "# test examples. -1 means use all."})
-    src_lang  = field(default=None, metadata={"help": "Source language id for translation."})
-    tgt_lang  = field(default=None, metadata={"help": "Target language id for translation."})
-    eval_beams  = field(default=None, metadata={"help": "# num_beams to use for evaluation."})
+    def __init__(self):
+        self.data_dir = ""
+        self.task = "summarization"
+        self.max_source_length = 1024
+        self.max_target_length  = 128
+        self.val_max_target_length  = 142
+        self.test_max_target_length = 142
+        self.n_train  = -1
+        self.n_val  = -1
+        self.n_test  = -1
+        self.src_lang  = None
+        self.tgt_lang  = None
+        self.eval_beams  = None
