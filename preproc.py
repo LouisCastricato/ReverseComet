76e609dfe90ea3d7fe98a2629cb9bd4851380113
Louis <lcastric@edu.uwaterloo.ca>
2020-12-08 17:07:30 -0500

    multithreaded preproc

--- preproc.py
+++ preproc.py
@@ -3,27 +3,36 @@ from nltk.tokenize import sent_tokenize
 import numpy as np
 import csv
 from tqdm import tqdm
+import multiprocessing
 
 data = load_dataset("pg19", cache_dir= "cache/")
 
 train = list()
 val = list()
 
-for book in tqdm(data['train']['text']):
-    txt = sent_tokenize(book)[:-10]
+def preproc(inp):
+    txt = sent_tokenize(inp)[:-10]
 
     n_sent = len(txt)
-    n_start = np.random.randint(low = min(10, n_sent), high = n_sent)
-    n_length = np.random.randint(low = min(3, n_sent), high = min(15, n_sent))
-    n_prompt = np.random.randint(low = 1, high = 3)
+    try:
+        n_start = np.random.randint(low = min(10, n_sent), high = n_sent)
+        n_length = np.random.randint(low = min(3, n_sent), high = min(15, n_sent))
+        n_prompt = np.random.randint(low = 1, high = 3)
 
-    exmpl = txt[n_start - n_length:n_start + 1]
-    body = " ".join(exmpl[n_prompt:])
-    prompt = " ".join(exmpl[:n_prompt])
+        exmpl = txt[n_start - n_length:n_start + 1]
+        body = " ".join(exmpl[n_prompt:])
+        prompt = " ".join(exmpl[:n_prompt])
 
-    tgt = prompt
-    src = body + " <pmpt> "
-    train.append([src, tgt])
+        tgt = prompt
+        src = body + " <pmpt> "
+    except:
+        tgt = "None"
+        src = "None"
+    return [src, tgt]
+
+pool = multiprocessing.Pool(processes=8)
+train = pool.map(preproc, data['train']['text'])
+
 
 with open("train.csv", "wb") as f:
     writer = csv.writer(f)
@@ -31,24 +40,14 @@ with open("train.csv", "wb") as f:
 
 print("SAVED TRAINING SET")
 
-for book in tqdm(data['train']['text']):
-    txt = sent_tokenize(book)[:-10]
+val = pool.map(preproc, data['validation']['text'])
 
-    n_sent = len(txt)
-    n_start = np.random.randint(low = min(10, n_sent), high = n_sent)
-    n_length = np.random.randint(low = min(3, n_sent), high = min(15, n_sent))
-    n_prompt = np.random.randint(low = 1, high = 3)
 
-    exmpl = txt[n_start - n_length:n_start + 1]
-    body = " ".join(exmpl[n_prompt:])
-    prompt = " ".join(exmpl[:n_prompt])
-
-    tgt = prompt
-    src = body + " <pmpt> "
-    train.append([src, tgt])
-
-with open("eval.csv", "wb") as f:
+with open("validation.csv", "wb") as f:
     writer = csv.writer(f)
-    writer.writerows(train)
-    
-print("SAVED EVAL")
+    writer.writerows(val)
+
+print("SAVED EVAL SET")
+
+
+pool.close()
