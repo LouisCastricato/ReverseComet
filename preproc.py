2749364d05d25bbf9e985432e4338340632919e9
Louis <lcastric@edu.uwaterloo.ca>
2020-12-08 22:23:08 -0500

    fixed preproc

--- preproc.py
+++ preproc.py
@@ -5,7 +5,7 @@ import csv
 from tqdm import tqdm
 import multiprocessing
 import pandas as pd
-
+from tqdm import tqdm
 data = load_dataset("pg19", cache_dir= "cache/")
 
 val = list()
@@ -28,16 +28,44 @@ def preproc(inp):
         tgt = "None"
         src = "None"
     return [src, tgt]
+
+def preproc_from_sents(inp):
+    try:
+        txt = inp
+        n_sent = len(txt)
+        n_start = np.random.randint(low = min(10, n_sent), high = n_sent)
+        n_length = np.random.randint(low = min(3, n_sent), high = min(15, n_sent))
+        n_prompt = np.random.randint(low = 1, high = 3)
+
+        exmpl = txt[n_start - n_length:n_start + 1]
+        body = " ".join(exmpl[n_prompt:]).replace("\n", " ")
+        prompt = " ".join(exmpl[:n_prompt]).replace("\n", " ")
+
+        tgt = prompt
+        src = body + " <pmpt> "
+    except:
+        tgt = "None"
+        src = "None"
+    return [src, tgt]
+
+
 #Double up the lists, since its randomly sampled we should get zero overlap
 train_input = data['train']['text']
-for i in range(13):
-    train_input += data['train']['text']
-
 pool = multiprocessing.Pool(processes=8)
-train = pd.DataFrame(pool.map(preproc, train_input))
-train.to_csv('train.csv', index=False, header=False)
+print(" TOKENIZING ")
+train_sent_tokenized = pool.map(lambda x: sent_tokenize(x)[10:-30], train_input)
 
-print("SAVED TRAINING SET")
+print(" RANDOMIZING ")
+train_df = None
+for i in tqdm(range(15)):
+    if i == 0:
+        train_df = pd.DataFrame(pool.map(preproc_from_sents, train_input))
+    else:
+        train_df.append(pd.DataFrame(pool.map(preproc_from_sents, train_input)))
+
+train_df.to_csv('train.csv', index=False, header=False)
+
+print(" SAVED TRAINING SET ")
 
 val_input = data['validation']['text']
 for i in range(5):
